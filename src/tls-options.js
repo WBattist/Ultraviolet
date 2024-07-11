757f4de54dd66bd948f2ea7352cc844e4cfb77f2
WBattist <144733159+WBattist@users.noreply.github.com>
2024-07-11 19:24:31 -0400

    Delete src/tls-options.js

--- src/tls-options.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-module.exports = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert'),
-    ciphers: 'ECDHE-RSA-AES128-GCM-SHA256:!RC4:!MD5:!aNULL',
-    honorCipherOrder: true
-};
